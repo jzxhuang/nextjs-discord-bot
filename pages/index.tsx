858fe282ebebed88a5a32f903f3a5ce2a2e73ea3
Jeffrey Huang <jeff.huang62@gmail.com>
2021-04-29 19:17:08 -0400

    Render a table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,10 @@
 import React from "react"
 import { InferGetServerSidePropsType } from "next"
-import { createGlobalCommand, getGlobalCommands } from "services/discord"
+import { getGlobalCommands } from "services/discord"
 
 export const getServerSideProps = async () => {
   try {
     const { data } = await getGlobalCommands()
-    // await createGlobalCommand({
-    //   name: "randompic",
-    //   description: "Get a random picture",
-    //   options: [
-    //     {
-    //       name: "type",
-    //       description: "What type of picture would you like?",
-    //       type: 3,
-    //       required: true,
-    //       choices: [
-    //         { name: "cat", value: "cat" },
-    //         { name: "dog", value: "dog" },
-    //         { name: "generic", value: "picsum" },
-    //       ],
-    //     },
-    //   ],
-    // })
     return { props: { data } }
   } catch (err) {
     console.error(err)
@@ -30,9 +13,41 @@ export const getServerSideProps = async () => {
 }
 
 const IndexPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log("data:", data)
-
-  return <div>Hello World</div>
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h2>i.e. you could build a dashboard here... we'll just fetch all the slash commands the bot has registered</h2>
+      {data && data.length > 0 ? (
+        <div>
+          <h3>All commands</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>description</th>
+                <th>default permission</th>
+                <th>options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((command) => (
+                <tr key={command.id}>
+                  <td>{command.id} </td>
+                  <td>{command.name} </td>
+                  <td>{command.description} </td>
+                  <td>{String(command.default_permission)}</td>
+                  <td>{command.options ? <div>{JSON.stringify(command.options[0])}</div> : "No options"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        "No commands found"
+      )}
+    </div>
+  )
 }
 
 export default IndexPage
